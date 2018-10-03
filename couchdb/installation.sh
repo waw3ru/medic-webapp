4401fa5cd17a21f5f2e3b1b118fdec7f67e34704
John ⚒ Waweru <wambugu.john.waweru@outlook.com>
2018-10-03 10:06:32 +0300

    Update installation.sh
    fixed issue with the port defined on couch/installation.sh

--- couchdb/installation.sh
+++ couchdb/installation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo http://$1:$2@127.0.0.1:5985/_global_changes
+echo http://$1:$2@127.0.0.1:5984/_global_changes
 
-curl -X PUT http://$1:$2@127.0.0.1:5985/_users
-curl -X PUT http://$1:$2@127.0.0.1:5985/_replicator
-curl -X PUT http://$1:$2@127.0.0.1:5985/_global_changes
+curl -X PUT http://$1:$2@127.0.0.1:5984/_users
+curl -X PUT http://$1:$2@127.0.0.1:5984/_replicator
+curl -X PUT http://$1:$2@127.0.0.1:5984/_global_changes
 
-curl -X POST http://$1:$2@127.0.0.1:5985/_users \
+curl -X POST http://$1:$2@127.0.0.1:5984/_users \
   -H "Content-Type: application/json" \
   -d '{ "_id": "org.couchdb.user:admin", "name": "'$1'", "password":"'$2'", "type":"user", "roles":[] }'
 
-curl -X PUT http://$1:$2@127.0.0.1:5985/_node/couchdb@$3/_config/httpd/secure_rewrites \
+curl -X PUT http://$1:$2@127.0.0.1:5984/_node/couchdb@$3/_config/httpd/secure_rewrites \
   -d '"false"' -H "Content-Type: application/json"
